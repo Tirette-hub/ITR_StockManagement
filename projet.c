3e259a8736493274100bdc9c284fa68f4da68fac
Tirette-hub <nathan.amorison@gmail.com>
2022-05-21 15:24:39 +0200

    adding Stock Manager stocks creation

--- projet.c
+++ projet.c
@@ -36,6 +36,8 @@
 
 #define true 1
 #define false 0;
+#define product_number 4
+#define client_number 2
 typedef bool int;
 
 //RT signals
@@ -44,21 +46,21 @@ typedef bool int;
 #define SIGRTR (SIGRTMIN + 0) //SIGnal Real Time "Ready"	//from client to stock manager
 
 //data structures
-typedef struct{
+typedef struct Product{
 	int id,
 	volume;
 	char descr[256];
-}Product;
-typedef struct{
+} Product;
+typedef struct Productor{
 	int product_id;
 	double production_time;
-}Productor;
-typedef struct{
+} Productor;
+typedef struct Client{
 	int id,
 	min_time,
 	max_time,
 	*request;
-}Client;
+} Client;
 
 //behavior descriptions
 void* ProductorBehavior(void*);
@@ -67,10 +69,13 @@ void* ClientBehavior(void*);
 
 //handlers
 void handleQuit(int signum);
+void handleFullProductorStock(int, siginfo_t*, void*);
+void handleOrder(int, siginfo_t*, void*);
 
 //getters
 int getDeltaMili();
 bool isDayTime();
+int roundStock(double, int);
 
 //setters
 void createDataSet();
@@ -78,9 +83,13 @@ void createDataSet();
 
 //global variables
 pid_t other_pid;
-int quit = 0; product_number = 4, client_number = 2, count = 0;
+int quit = 0, count = 0;
 sem_t* semaphore;
 int product_size = sizeof(Product), productor_size = sizeof(Productor), client_size = sizeof(Client);
+
+//global variables only used by stock manager process
+int shmid_tab[product_number];
+int order_queue[client_number];
 
 volatile sig_atomic_t status;
 sigset_t mask;
@@ -88,6 +97,8 @@ sigset_t mask;
 Product products[product_number];
 Productor productors[product_number];
 Client clients[client_number];
+int *stocks[product_number];
+int stocks_parameters[2*product_number]; //4 first int: max size of each stock, 4 last int: current filling level
 clock_t begin;
 
 int main(int argc, char* argv[]){
@@ -149,6 +160,8 @@ void* ProductorBehavior(void* unused){
 	Productor self = productors[count++];
 	sem_post(semaphore);
 
+	envelope.sival_int = self.product_id;
+
 	//create local stock
 	shmid = shmget(ipc_key, 2*sizeof(int), IPC_CREAT | 0600); //check how to create the unique id for product identification
 	//link shm addresses to pointers
@@ -184,6 +197,64 @@ void* ProductorBehavior(void* unused){
 
 void ManagerBehavior(){
 	srand(time(NULL));
+
+	//creating stocks
+	int max_stock_volume = 100; //total volume
+
+	//what we want to perform modularly from this data set
+	/*int apple_stock[34];	//34V
+	int pear_stock[23];		//23V
+	int wood_stock[7];		//21V
+	int brick_stock[11];	//22V //total = 100V
+	stocks[0] = apple_stock;
+	stocks[1] = pear_stock;
+	stocks[2] = wood_stock;
+	stocks[3] = brick_stock;*/
+
+	//some math stuff while creating optimized stocks to do as described just befores
+	int devider = 0;
+	int checker = 0; //debug variable
+	for (int i = 0; i < product_number; i++){
+		//get production for a day:
+		double day_production = DAY_DURATION*products[productors[i].product_id].volume*UT/(1000.0*productors[i].production_time);
+		devider = devider + day_production;
+	}
+	//partitionning
+	for (int i = 0; i < product_number; i++){
+		int product_volume = products[productors[i].product_id].volume;
+		double rebasement = DAY_DURATION*product_volume*UT*max_stock_volume/(1000.0*devider*productors[i].production_time); //change "percentage base" of day production
+		int stock_base = (int)(rebasement - fmod(rebasement, product_volume));
+		int stock_size = stock_base + roundStock(rebasement, product_volume);
+		checker = checker + stock_size;
+		int stock[stock_size/product_volume];
+		stocks_parameters[i] = stock_size;
+		stocks_parameters[i+product_number] = 0;
+		stocks[i] = stock;
+	}
+
+	//creating shm
+	for (int i = 0; i < product_number; i++)
+		shmid_tab[i] = shmget(ftok("projet.c", i), 2*sizeof(int), IPC_CREAT | 0600); //get the shm id
+
+	//create message queues to communicate with clients
+
+	//expect signals from Productors or Clients
+	struct sigaction descriptor;
+	descriptor.sa_flags=SA_SIGINFO;
+	descriptor.sa_sigaction = handleProductor;
+	sigaction(SIGRTF, &descriptor, NULL);
+	descriptor.sa_sigaction = handleClient;
+	sigaction(SIGRTR, &descriptor, NULL);
+
+	//principle loop
+	while(!quit){
+
+	}
+
+	//free shm
+	for (int i = 0; i < product_number; i++){
+		shmctl(shmid, IPC_RMID, NULL);
+	}
 }
 
 void* ClientBehavior(void* unused){
@@ -229,69 +300,78 @@ bool isDayTime(){
 	return false;
 }
 
+int roundStock(double base, int product_volume){
+	double rest = fmod(base, product_volume);
+	if (rest <= product_volume / 2.0)
+		return 0;
+	return product_volume;
+}
+
 
 //setters
 
 void createDataSet(){
 	//create products
-	Product pomme = {
-		0;
-		1;
-		"An apple every day keeps doctors away";
-	}
-	Product poire = {
-		1;
-		1;
-		"La bonne poire";
-	}
-	Product bois = {
-		2;
-		3;
-		"Un tronc tout frais";
-	}
-	Product brique = {
-		3;
-		2;
-		"Utilisé dans la construction de maisons";
-	}
-	products[0] = pomme;
-	products[1] = poire;
-	products[2] = bois;
-	products[3] = brique;
+	Product apple = {
+		0,
+		1,
+		"An apple every day keeps doctors away"
+	};
+	Product pear = {
+		1,
+		1,
+		"La bonne pear"
+	};
+	Product wood = {
+		2,
+		3,
+		"Un tronc tout frais"
+	};
+	Product brick = {
+		3,
+		2,
+		"Utilisé dans la construction de maisons"
+	};
+	products[0] = apple;
+	products[1] = pear;
+	products[2] = wood;
+	products[3] = brick;
 
 	//create productors
-	Productor pommiers = {
-		0;
-		1;
-	}
-	Productor poiriers = {
-		1;
-		1.5;
-	}
-	Productor foret = {
-		2;
-		5;
-	}
-	Productor carriere = {
-		3;
-		3;
-	}
-	productors[0] = pommiers;
-	productors[1] = poiriers;
-	productors[2] = foret;
-	productors[3] = carriere;
+	Productor apple_trees = {
+		0,
+		1
+	};
+	Productor pear_trees = {
+		1,
+		1.5
+	};
+	Productor forest = {
+		2,
+		5
+	};
+	Productor quarry = {
+		3,
+		3
+	};
+	productors[0] = apple_trees;
+	productors[1] = pear_trees;
+	productors[2] = forest;
+	productors[3] = quarry;
 
 	//create clients
-	Client maraicher = {
-		0;
-		2;
-		10;
-		{2, pomme, 2, poire, 3};
-	}
-	Client macon = {
-		1;
-		5;
-		15;
-		{2, bois, 1, brique, 5};
-	}
+	Client market = {
+		0,
+		2,
+		10,
+		{2, apple, 2, pear, 3}
+	};
+	Client mason = {
+		1,
+		5,
+		15,
+		{2, wood, 1, brick, 5}
+	};
+	clients[0] = market;
+	clients[1] = mason;
 }
